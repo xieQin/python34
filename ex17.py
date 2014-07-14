732aac783248276c9a75e674f21592ee0e954417
xieQin <xieqin1991@gmail.com>
2014-07-14 17:14:43 +0800

    add python ex15 to ex31

---
+++ ex17.py
@@ -0,0 +1,23 @@
+from sys import argv
+from os.path import exists
+
+script, from_file, to_file = argv
+
+print("Coping from %s to %s" %(from_file,to_file))
+
+in_file = open(from_file)
+indata = in_file.read()
+
+print("The input file is %d bytes long" %len(indata))
+
+print("Does the output file exists? %r" %exists(to_file))
+print("Ready, hit RETURN to continue, CTRL-C to abort")
+input()
+
+out_file = open(to_file,'w')
+out_file.write(indata)
+
+print("Alright, all done.")
+
+out_file.close()
+in_file.close()
