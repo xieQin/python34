c583549a83c54dfe0e06e13a284ca59b61336b18
xieQin <xieqin1991@gmail.com>
2014-07-30 15:06:36 +0800

    modify urllib_test.py

--- urllib_test.py
+++ urllib_test.py
@@ -2,3 +2,9 @@ from urllib.request import urlopen
 response = urlopen('http://www.baidu.com')
 html = response.read()
 print(html)
+
+# import urllib2
+# req = urllib2.request('http://www.baidu.com')
+# response = urllib2.urlopen(req)
+# the_page = response.read()
+# print(the_page)
